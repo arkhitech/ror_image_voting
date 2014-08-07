6a43ca4fe2f725f5e17f10c6a08e749edb963c82
Mudasir Raza <mudasir.raza@arkhitech.com>
2014-08-07 16:09:51 +0500

    deploy minor change

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,7 +2,7 @@
 lock '3.2.1'
 
 set :application, 'ror_image_voting'
-set :repo_url, 'git@example.com:me/my_repo.git'
+set :repo_url, 'git@github.com:arkhitech/ror_image_voting.git'
 
 # Default branch is :master
 # ask :branch, proc { `git rev-parse --abbrev-ref HEAD`.chomp }.call

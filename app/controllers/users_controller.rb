5322f349f4169f721aa9d1d7c7cef224b3edfb60
Mudasir Raza <mudasir.raza@arkhitech.com>
2014-08-21 18:40:10 +0500

    issues fixed

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < InheritedResources::Base
+    skip_before_filter :verify_authenticity_token,
+                     if: Proc.new { |c| c.request.format == 'application/json' }
 
   before_filter :authenticate_user_from_token!
   
